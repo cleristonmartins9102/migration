2ea276a3fd700a06829666a00632600abf9c12be
Martins Cleriston <cleriston.mari@gmail.com>
2024-08-07 10:33:11 +0100

    feat: add update member push settings router

--- src/main/routers/update/update-member-settings-router.ts
+++ src/main/routers/update/update-member-settings-router.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { expressAdapter } from '@/main/adapters';
-import { updateSmsSettingsControllerAdapterFactory } from '@/main/factories/controller/update/update-sms-settings-controller-factory';
+import { updatePushSettingsControllerAdapterFactory, updateSmsSettingsControllerAdapterFactory } from '@/main/factories/controller';
 
 export const updateMemberSettingsRouter = (router: Router): void => {
   router.post('/update-sms-notifications', expressAdapter(updateSmsSettingsControllerAdapterFactory()))
+  router.post('/update-push-notifications', expressAdapter(updatePushSettingsControllerAdapterFactory()))
 }
