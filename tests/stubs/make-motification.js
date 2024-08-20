e22bcd70b715a6d51f52d2a1d37a08f37046ef45
Martins Cleriston <cleriston.mari@gmail.com>
2024-08-20 13:50:55 +0100

    feat: remove js files

--- tests/stubs/make-motification.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeFakeMotification = void 0;
-var makeFakeMotification = function () { return ({
-    product_id: '1',
-    image: 'http',
-    name: 'any product'
-}); };
-exports.makeFakeMotification = makeFakeMotification;
